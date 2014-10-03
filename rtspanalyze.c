eaaa7cc255a5c62e1b92a0ccf39f273afb37f3ec
Remy Rojas <rrojasdela92@gmail.com>
2014-10-03 16:23:12 +0300

    rtsp receiving ok

---
+++ rtspanalyze.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include "rtspanalyze.h"
+
+// Local function:
+void replace_return_with_null(char*);
+
+/*
+ * rtspanalyze -- search for RTSP protocol data from a buffer
+ * It is assumed that the buffer contains only one method (not
+ * pipelined instructions, where there may be many).
+ * Sets the "method" integer field in the structure.
+ * Sets those header field pointers that are found.
+ * Returns 0 if the method was OK, otherwise nonzero.
+ */
+int rtspanalyze(char* p_buf, Rtspblock* p_data)
+{
+  int retval = 0;
+  char* ptr;
+
+  if (!p_buf || !p_data)
+    return -1;
+
+  bzero(p_data, sizeof(Rtspblock));
+
+  // Put the method into a number
+  if ((ptr = strstr(p_buf, "OPTIONS")) != NULL)
+    p_data->method = OPTIONS;
+  else if ((ptr = strstr(p_buf, "DESCRIBE")) != NULL)
+    p_data->method = DESCRIBE;
+  else if ((ptr = strstr(p_buf, "ANNOUNCE")) != NULL)
+    p_data->method = ANNOUNCE;
+  else if ((ptr = strstr(p_buf, "SETUP")) != NULL)
+    p_data->method = SETUP;
+  else if ((ptr = strstr(p_buf, "PLAY")) != NULL)
+    p_data->method = PLAY;
+  else if ((ptr = strstr(p_buf, "PAUSE")) != NULL)
+    p_data->method = PAUSE;
+  else if ((ptr = strstr(p_buf, "TEARDOWN")) != NULL)
+    p_data->method = TEARDOWN;
+  else if ((ptr = strstr(p_buf, "GET_PARAMETER")) != NULL)
+    p_data->method = GET_PARAMETER;
+  else if ((ptr = strstr(p_buf, "SET_PARAMETER")) != NULL)
+    p_data->method = SET_PARAMETER;
+  else if ((ptr = strstr(p_buf, "REDIRECT")) != NULL)
+    p_data->method = REDIRECT;
+  else if ((ptr = strstr(p_buf, "RECORD")) != NULL)
+    p_data->method = RECORD;
+  else
+    return 1;
+
+  // Search for headers. Brute-force search that does not take into account
+  // the fact that "Require:" is a substring of "Proxy-Require:"
+  p_data->conn = strstr(p_buf,"Connection:");
+  p_data->contenc = strstr(p_buf,"Content-Encoding:");
+  p_data->contlang = strstr(p_buf,"Content-Language");
+  p_data->contlen = strstr(p_buf,"Content-Length:");
+  p_data->conttype = strstr(p_buf,"Content-Type:");
+  p_data->cseq = strstr(p_buf,"CSeq:");
+  p_data->proxyrequire = strstr(p_buf,"Proxy-Require:");
+  p_data->require = strstr(p_buf,"Require:");// <---- DOES NOT WORK, needs logic
+  p_data->rtpinfo = strstr(p_buf,"RTP-Info:");
+  p_data->session = strstr(p_buf,"Session:");
+  p_data->transport = strstr(p_buf,"Transport:");
+  p_data->unsupp = strstr(p_buf,"Unsupported:");
+
+  // Adjust the pointer and null-terminate the headers.
+  if (p_data->conn)
+    {
+      replace_return_with_null(p_data->conn);
+      p_data->conn += strlen("Connection: ");
+    }
+  if (p_data->contenc)
+    {
+      replace_return_with_null(p_data->contenc);
+      p_data->contenc += strlen("Content-Encoding: ");
+    }
+  if (p_data->contlang)
+    {
+      replace_return_with_null(p_data->contlang);
+      p_data->contlang += strlen("Content-Language: ");
+    }
+  if (p_data->contlen)
+    {
+      replace_return_with_null(p_data->contlen);
+      p_data->contlen += strlen("Content-Length: ");
+    }
+  if (p_data->conttype)
+    {
+      replace_return_with_null(p_data->conttype);
+      p_data->conttype += strlen("Content-Type: ");
+    }
+  if (p_data->cseq)
+    {
+      replace_return_with_null(p_data->cseq);
+      p_data->cseq += strlen("CSeq: ");
+    }
+  if (p_data->proxyrequire)
+    {
+      replace_return_with_null(p_data->proxyrequire);
+      p_data->proxyrequire += strlen("Proxy-Require: ");
+    }
+  if (p_data->require)
+    {
+      replace_return_with_null(p_data->require);
+      p_data->require += strlen("Require: ");
+    }
+  if (p_data->rtpinfo)
+    {
+      replace_return_with_null(p_data->rtpinfo);
+      p_data->rtpinfo += strlen("RTP-Info: ");
+    }
+  if (p_data->session)
+    {
+      replace_return_with_null(p_data->session);
+      p_data->session += strlen("Session: ");
+    }
+  if (p_data->transport)
+    {
+      replace_return_with_null(p_data->transport);
+      p_data->transport += strlen("Transport: ");
+    }
+  if (p_data->unsupp)
+    {
+      replace_return_with_null(p_data->unsupp);
+      p_data->unsupp += strlen("Unsupported: ");
+    }
+  /* Check if required fields were found. Set URL pointer by adding
+   * the method string length + 1 (space) to the method pointer value.
+   */
+  switch (p_data->method)
+    {
+    case OPTIONS:
+      p_data->url = ptr + 8;
+      break;
+    case DESCRIBE:
+      p_data->url = ptr + 9;
+      break;
+    case ANNOUNCE:
+      p_data->url = ptr + 9;
+      break;
+    case SETUP:
+      p_data->url = ptr + 6;
+      break;
+    case PLAY:
+      p_data->url = ptr + 5;
+      break;
+    case PAUSE:
+      p_data->url = ptr + 6;
+      break;
+    case TEARDOWN:
+      p_data->url = ptr + 9;
+      break;
+    case GET_PARAMETER:
+      p_data->url = ptr + 14;
+      break;
+    case SET_PARAMETER:
+      p_data->url = ptr + 14;
+      break;
+    case REDIRECT:
+      p_data->url = ptr + 9;
+      break;
+    case RECORD:
+      p_data->url = ptr + 7;
+      break;
+
+    case 0: // never, if handled above already
+    default:
+      return 1;
+    }
+  // Null-terminate the URL:
+  if ((ptr = strstr(ptr, "RTSP/1.0")) != NULL)
+    *(ptr - 1) = '\0';
+
+  return retval;
+}
+
+void replace_return_with_null(char *p_str)
+{
+  char *retplace;
+  if ((retplace = strchr(p_str, '\n')) != NULL)
+    *retplace = '\0';
+}
---
+++ serveur.c
@@ -0,0 +1,116 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+
+#include "rtspanalyze.h"
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc,char* argv[]){
+
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[5000];
+    /**rtsp analyze variables**/
+    int result;
+    Rtspblock rtspdata;
+    char *buf;
+    buf = malloc(sizeof(char)* (strlen(buffer)+1));
+    /*************************/
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+       error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+             error("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+
+	int rc;
+	//CONNECTION
+	int i=0;
+	//for(;;){
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(sockfd,&readfds);
+		rc=select(6,&readfds,NULL,NULL,NULL);
+
+		if(FD_ISSET(sockfd,&readfds)) {
+			int acc=accept(sockfd,(struct sockaddr*)&cli_addr,&clilen);
+
+			int rcv,snd;
+            /*if((rcv = recvfrom(acc,buffer,sizeof(buffer),0,(struct sockaddr *)&cli_addr,&clilen))<0) error("rcv error\n");
+            printf("%s\n",buffer);
+            printf("BUFFER END\n\n");//OPTIONS
+            if((snd = send(acc,buffer,sizeof(buffer),0))<0) error("send error\n");
+            bzero(buffer,5000);
+*/
+			int fid=fork();
+			if(fid>0) {//father
+                printf("i'm the father i close acc and keep sockfd%d\n", acc);
+                close(acc);
+				//printf("fid=%d\n",fid);
+				//printf("acc = %d\n", acc);
+				//printf("client IP: %d\nclient Port: %d\n",cli_addr.sin_addr,serv_addr.sin_port);
+			}
+			if(fid==0){//son
+                printf("i'm the son i close sockfd and keep acc\n");
+                close(sockfd);
+				int i;
+                int rcv,snd;
+
+                for ( ; ; )
+                {
+                    /**receive instruction message*/
+                    if((rcv = recvfrom(acc,buffer,sizeof(buffer),0,(struct sockaddr *)&cli_addr,&clilen))<0) error("rcv error\n");
+                    /** Determine the instruction*/
+                    strcpy(buf,buffer);
+                    printf("received: \n%s\n",buf);//receive buffer, copy into buf... great variable names!!
+                    if((result=rtspanalyze(buf,&rtspdata))!=0) error("rtspanalyze error!!\n");
+                    else {
+                    /**DECIDE WHAT TO DO*/
+                        switch(rtspdata.method){
+                            case OPTIONS:
+                                if((snd = send(acc,buffer,sizeof(buffer),0))<0) error("send error\n");
+                                if((rcv = recvfrom(acc,buffer,sizeof(buffer),0,(struct sockaddr *)&cli_addr,&clilen))<0) error("rcv error\n");
+                                if((snd = send(acc,buffer,sizeof(buffer),0))<0) error("send error\n");
+                                /**little error 405 but aparently doesn't prevent the describe from being sent later on*/
+                                break;
+                            case DESCRIBE:
+
+                                break;
+                            case PLAY:
+                                break;
+                        }
+                    bzero(buf,sizeof(buf));
+                    bzero(buffer,sizeof(buffer));
+                    }
+                }
+
+			}
+		//}
+	}
+
+}
