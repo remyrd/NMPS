198edd6e35b459bd0d76a2a87494788757370ada
Remy Rojas <rrojasdela92@gmail.com>
2014-10-04 18:34:35 +0300

    Jussi correction on add_to_buffer

---
+++ rtspserver.c
@@ -0,0 +1,168 @@
+/* This is serveur.c modified 2014-10-04 (not in version control):
+ * - Jussi's experimental modification concerning DESCRIBE
+ * Also, error() renamed in order to not conflict with lib func error(3),
+ * now it is err_exit()
+ * And, modified second argument of add_to_buffer to be a const array
+ */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+#include "rtspanalyze.h"
+
+void err_exit(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+void add_to_buffer(char *dest, const char *src,bool endline){
+    strcat(dest,src);
+    if (endline) strcat(dest,"\r\n");
+}
+
+int main(int argc,char* argv[]){
+
+  int sockfd, newsockfd, portno;
+  socklen_t clilen;
+
+  char buffer[5000], sdpbuff[200]; // sdplen[5];
+
+  /**rtsp analyze variables**/
+  int result;
+  Rtspblock rtspdata;
+  char *buf;
+
+  const char remy_SDP1[] = 
+    "v=0\n"
+    "o=remy 123456789 987654321 IN IP4 10.0.2.15\n"
+    "s=nmps-session\n"
+    "c=IN IP4 10.0.2.15\n"
+    "t=0 0\n"
+    "m=video 3001 RTP/AVP 31\r\n"; // strings can be broken into several lines 
+
+  buf = malloc(sizeof(char)* (strlen(buffer)+1));
+  /*************************/
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
+
+
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);
+  }
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    err_exit("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+           sizeof(serv_addr)) < 0)
+    err_exit("ERROR on binding");
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+
+  int rc;
+  //CONNECTION
+  int i=0;
+  //for(;;){
+  fd_set readfds;
+  FD_ZERO(&readfds);
+  FD_SET(sockfd,&readfds);
+  rc=select(6,&readfds,NULL,NULL,NULL);
+
+  if(FD_ISSET(sockfd,&readfds)) {
+    int acc=accept(sockfd,(struct sockaddr*)&cli_addr,&clilen);
+
+    int rcv,snd;
+    /*if((rcv = recvfrom(acc,buffer,sizeof(buffer),0,(struct sockaddr *)&cli_addr,&clilen))<0) err_exit("rcv error\n");
+      printf("%s\n",buffer);
+      printf("BUFFER END\n\n");//OPTIONS
+      if((snd = send(acc,buffer,sizeof(buffer),0))<0) err_exit("send error\n");
+      bzero(buffer,5000);
+    */
+    int fid=fork();
+    if(fid>0) {//father
+      printf("i'm the father i close acc and keep sockfd%d\n", acc);
+      close(acc);
+      //printf("fid=%d\n",fid);
+      //printf("acc = %d\n", acc);
+      //printf("client IP: %d\nclient Port: %d\n",cli_addr.sin_addr,serv_addr.sin_port);
+    }
+    if(fid==0){//son
+      printf("i'm the son i close sockfd and keep acc\n");
+      close(sockfd);
+      int i;
+      int rcv,snd;
+
+      for ( ; ; )
+        {
+          /**receive instruction message*/
+          if((rcv = recvfrom(acc,buffer,sizeof(buffer),0,(struct sockaddr *)&cli_addr,&clilen))<0) err_exit("rcv error\n");
+          /** Determine the instruction*/
+          strcpy(buf,buffer);
+          printf("received: \n%s\n",buf);//receive buffer, copy into buf... great variable names!!
+          if((result=rtspanalyze(buf,&rtspdata))!=0) err_exit("rtspanalyze error!!\n");
+          else {
+            /**DECIDE WHAT TO DO*/
+            switch(rtspdata.method){
+            case OPTIONS:
+              bzero(buffer,sizeof(buffer));
+              add_to_buffer(buffer,"RTSP/1.0 200 OK",true);
+              add_to_buffer(buffer,"Cseq: ",false);
+              add_to_buffer(buffer,rtspdata.cseq,true);
+              add_to_buffer(buffer,"Public: DESCRIBE, SETUP, TEARDOWN, PLAY, PAUSE\r\n",true);
+              printf("about to send:\n%s",buffer);
+              if((snd = send(acc,buffer,sizeof(buffer),0))<0) err_exit("send error\n");
+              /*Response    =     Status-Line
+               *(    general-header
+               |     response-header
+               |     entity-header )
+               CRLF
+               [ message-body ]
+
+               Status-Line =   RTSP-Version SP Status-Code SP Reason-Phrase CRLF
+               break;*/
+
+            case DESCRIBE:
+              // err_exit("this case doesn't work properly\n");
+              bzero(buffer,sizeof(buffer));
+	      add_to_buffer(buffer,"RTSP/1.0 200 OK",true);
+	      add_to_buffer(buffer,"CSeq: ",false);
+	      add_to_buffer(buffer,rtspdata.cseq,true);
+	      add_to_buffer(buffer,"Content-Type: ",false);
+	      add_to_buffer(buffer,rtspdata.conttype,true);
+	      bzero(sdpbuff,sizeof(sdpbuff));
+	      sprintf(sdpbuff,"Content-Length: %d\n",strlen(remy_SDP1));
+	      add_to_buffer(sdpbuff,remy_SDP1,false);
+	      add_to_buffer(buffer,sdpbuff,false); // remy_SDP1 ends in \n
+	      printf("about to send:\n%s",buffer);
+	      if((snd = send(acc,buffer,sizeof(buffer),0))<0)
+		err_exit("send error\n");
+	      break;
+
+            case PLAY:
+              break;
+            }
+            bzero(buf,sizeof(buf));
+            bzero(buffer,sizeof(buffer));
+          }
+        }
+      
+    }
+    
+  }
+
+} // m a i n ()
